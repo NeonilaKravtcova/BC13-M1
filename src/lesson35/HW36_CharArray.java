9f91e9394bf9f678f046e4e7a62ad6c50be4a2fd
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2020-08-14 01:56:44 +0200

    HW 36, Cyrillic Random Array

--- src/lesson35/HW36_CharArray.java
+++ src/lesson35/HW36_CharArray.java
@@ -2,13 +2,38 @@ package lesson35;
 
 public class HW36_CharArray {
     public static void main(String[] args) {
-        ArrayCharCyrillicAlphabetInit();
+        System.out.println(arrayCyrillic());
+        System.out.println();
+        System.out.println(cyrillicRandom(arrayCyrillic()));
     }
 
-    private static char[] ArrayCharCyrillicAlphabetInit(){
-        char[] cyrillicAlphabet = new char[]{};
-
+    public static char[] arrayCyrillic() {
+        char[] cyrillicAlphabet = new char[33];
+        int n = "А".charAt(0);
+        for (int i = 0; i < 6; i++) {
+            cyrillicAlphabet[i] = (char) (n);
+            n++;
+        }
+        cyrillicAlphabet[6] = "Ё".charAt(0);
+        for (int i = 7; i < 33; i++) {
+            cyrillicAlphabet[i] = (char) (n);
+            n++;
+        }
         return cyrillicAlphabet;
     }
 
+    public static char[] cyrillicRandom(char[] input) {
+        char[] output = new char[input.length];
+        int counter = 0;
+        do {
+            int n = (int) (Math.random() * input.length);
+            if (input[n] != '*') {
+                output[counter] = input[n];
+                input[n] = '*';
+                counter++;
+            }
+        }
+        while (counter < input.length);
+        return output;
+    }
 }
